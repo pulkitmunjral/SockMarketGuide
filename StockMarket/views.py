9e9f111b9cd0747532ddc6992b95ddd98edd5fe1
qe13026445 <qe13026445@ldap.com>
2022-02-28 23:31:43 +0530

    added query page

--- StockMarket/views.py
+++ StockMarket/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm
-from django.contrib import messages
+from .forms import UserRegisterForm
 from .models import Stock, Query
 from django.http import JsonResponse
 from django.contrib import messages
